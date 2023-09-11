406152cdbe66c67e8d4627f3ce6a5877fb2525ae
dev.alfad <dev.alfad@gmail.com>
2023-09-11 16:29:40 +0700

    fix(routing dashboard and naming)

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
