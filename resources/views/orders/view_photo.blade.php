22a0f676df6297e391f7497776a19b60d2021d9e
dev.alfad <dev.alfad@gmail.com>
2023-09-11 12:21:22 +0700

    fix(view image)

--- resources/views/orders/view_photo.blade.php
+++ resources/views/orders/view_photo.blade.php
@@ -13,7 +13,6 @@
             </div>
             <div class="col-md-6">
                 <p>id : {{$data['id']}}</p>
-                <!-- <a href="{{ $data['link']}}" class="btn btn-success">Download</a> -->
             </div>
         </div>
     </div>
