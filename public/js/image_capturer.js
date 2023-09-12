5c1320ad6576bb8b7d7731e89480c380178c0c90
dev.alfad <dev.alfad@gmail.com>
2023-09-13 02:55:09 +0700

    fix(camera): facing mode

--- public/js/image_capturer.js
+++ public/js/image_capturer.js
@@ -1,18 +1,33 @@
 
 //Camera
-Webcam.set({
-    width: 900,
-    height: 700,
-    image_format: 'jpeg',
-    jpeg_quality: 90,
-    facingMode: { exact: 'environment' }
-});
+// var cameras = new Array(); //create empty array to later insert available devices
+// navigator.mediaDevices.enumerateDevices() // get the available devices found in the machine
+//     .then(function (devices) {
+//         devices.forEach(function (device) {
+//             var i = 0;
+//             if (device.kind === "videoinput") { //filter video devices only
+//                 cameras[i] = device.deviceId; // save the camera id's in the camera array
+//                 i++;
+//             }
+//         });
+//     })
 
-Webcam.attach( '#my_camera' );
-    function take_snapshot() {
-        Webcam.snap( 
-            function(data_uri) {
-                $('.image-tag').val(data_uri);
-                document.getElementById('results').innerHTML = '<img src="'+data_uri+'"/>';
+
+Webcam.set(
+    'constraints',
+    {
+        width: 900,
+        height: 700,
+        image_format: 'jpeg',
+        jpeg_quality: 90,
+        facingMode: { exact: 'environment' }
+    });
+
+Webcam.attach('#my_camera');
+function take_snapshot() {
+    Webcam.snap(
+        function (data_uri) {
+            $('.image-tag').val(data_uri);
+            document.getElementById('results').innerHTML = '<img src="' + data_uri + '"/>';
         });
-    }
+}
