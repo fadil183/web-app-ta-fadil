3301485e44508da7cbea93ad07357ddb6cac84f4
dev.alfad <dev.alfad@gmail.com>
2023-09-13 11:36:02 +0700

    test

--- public/js/code_scanner.js
+++ public/js/code_scanner.js
@@ -1,6 +1,5 @@
 //code scanner
 
-// const btnScan = document.getElementById('btnStartScanner');
 var inputElement = document.getElementById("order_id");
 
 function onScanSuccess(decodedText, decodedResult) {
@@ -8,19 +7,6 @@ function onScanSuccess(decodedText, decodedResult) {
   inputElement.value = `${decodedText}`;
   html5QrcodeScanner.clear();
 }
-
-// btnScan.addEventListener('click', event => {
-//   let html5QrcodeScanner = new Html5QrcodeScanner(
-//     "qr-reader",
-//     {
-//       fps: 15,
-//       qrbox: { width: 600, height: 200 },
-//       disableFlip: true,
-//       rememberLastUsedCamera: true,
-//     },
-//   /* verbose= */ true);
-//   html5QrcodeScanner.render(onScanSuccess);
-// });
 
 function startScan() {
   let html5QrcodeScanner = new Html5QrcodeScanner(
