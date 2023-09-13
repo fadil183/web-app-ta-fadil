e850b23875b648f2f6cba1f858988976cc11b042
dev.alfad <dev.alfad@gmail.com>
2023-09-13 12:50:49 +0700

    test

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -8,74 +8,85 @@
 
 
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Add New Product</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary form-control" href="{{ route('orders.index') }}"> Back</a>
-            </div>
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary form-control" href="{{ route('orders.index') }}"> Back</a>
         </div>
     </div>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+</div>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="{{ route('orders.store') }}" method="POST">
+    @csrf
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nomor pesanan / resi :</strong>
+                <input id="order_id" type="text" name="id_order" class="form-control" placeholder="nomor pesanan / nomor resi" value="">
+                <div id="qr-reader" style="width: 100%"></div>
+                <button id="btnStartScanner" class="form-control" type="button" value="Open Scanner">start scanner</button>
+            </div>
         </div>
-    @endif
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Bukti Gambar:</strong>
+                <div class="row">
+                    <!-- <div class="col-md-6">
+                        <input type="button" id="btnGetCamera" value="Check Camera">
+                        <select id="select"></select>
+                        <video id="my_camera" style="width: 700px; height: 900px;" autoplay playsinline></video>
+                        <br />
+                        <input type="hidden" name="image" class="image-tag">
 
-    <form action="{{ route('orders.store') }}" method="POST">
-    	@csrf
-
-         <div class="row">
-		    <div class="col-xs-12 col-sm-12 col-md-12">
-		        <div class="form-group">
-		            <strong>Nomor pesanan / resi :</strong> 
-		            <input id="order_id" type="text" name="id_order" class="form-control" placeholder="nomor pesanan / nomor resi" value="">
-                    <div id="qr-reader" style="width: 100%"></div> 
-                    <button id="btnStartScanner" class="form-control" type="button" value="Open Scanner">start scanner</button>
-		        </div>
-		    </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-		        <div class="form-group">
-		            <strong>Bukti Gambar:</strong>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <input type="button" id="btnGetCamera" value="Check Camera">
-                            <select id="select"></select>
-                            <video id="my_camera" style="width: 700px; height: 900px;" autoplay playsinline></video>
-                            <br/>
-                            <input type="hidden" name="image" class="image-tag" >
-                            
-                            <input class="form-control" type="button" value="Take Snapshot" onClick="captureImage()" >
-                        </div>
-                        <div class="col-md-6">
-                        <canvas id="canvas" width="640" height="480"></canvas>
-                            <div id="results" style="height: 100%;">Your captured image will appear here...</div>
-                        </div>
+                        <input class="form-control" type="button" value="Take Snapshot" onClick="captureImage()">
                     </div>
-		        </div>
-		    </div>
-		    <div class="col-xs-12 col-sm-12 col-md-12">
-		        <div class="form-group">
-		            <strong>Detail:</strong>
-		            <textarea class="form-control" style="height:150px" name="detail_order" placeholder="Detail"></textarea>
-		        </div>
-		    </div>
-		    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-		    <button type="submit" class="btn btn-primary form-control">Submit</button>
-		    </div>
-		</div>
+                    <div class="col-md-6">
+                        <canvas id="canvas" width="640" height="480"></canvas>
+                        <div id="results" style="height: 100%;">Your captured image will appear here...</div>
+                    </div> -->
+                    <main>
+                        <div class="controls">
+                            <button id="button">Get camera</button>
+                            <select id="select">
+                                <option></option>
+                            </select>
+                        </div>
 
-    </form>
+                        <video id="video" autoplay playsinline></video>
+                    </main>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Detail:</strong>
+                <textarea class="form-control" style="height:150px" name="detail_order" placeholder="Detail"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary form-control">Submit</button>
+        </div>
+    </div>
 
-    <script src="{{ asset('js/image_capturer.js')}}"></script>
-    <!-- <script src="{{ asset('js/code_scanner.js')}}"></script> -->
+</form>
+
+<!-- <script src="{{ asset('js/image_capturer.js')}}"></script> -->
+<script src="{{ asset('js/image_capturer_beta.js')}}"></script>
+
+<!-- <script src="{{ asset('js/code_scanner.js')}}"></script> -->
 
 @endsection
-
