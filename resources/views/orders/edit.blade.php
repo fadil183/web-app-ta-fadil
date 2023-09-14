9663cfca3d1e5b6b80b231a1e2fabe57f793be38
dev.alfad <dev.alfad@gmail.com>
2023-09-14 16:52:18 +0700

    test

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <script src="https://rawgit.com/serratus/quaggaJS/master/dist/quagga.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('content')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-right">
